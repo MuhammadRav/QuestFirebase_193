5e04119aa187236fcb0c8ea515606602a2771c4d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2025-01-12 14:17:01 +0700

    menambahkan Scaffold untuk tampilan InsertMahasiswaView

--- app/src/main/java/com/example/week13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/week13/ui/view/InsertView.kt
@@ -1,11 +1,20 @@
 package com.example.week13.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.week13.ui.viewModel.FormState
 import com.example.week13.ui.viewModel.InsertViewModel
@@ -49,6 +58,42 @@ fun InsertMahasiswaView(
                 }
             }
             else -> Unit
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") },
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding((16.dp))
+        ) {
+            InsertBodyMahasiswa(
+                uiState = uiEvent,
+                homeUiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFields()){
+                        viewModel.insertMahasiswa()
+                        //on navigasi()
+                    }
+                }
+            )
         }
     }
 }
