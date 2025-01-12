09941c6226f671fe02ace18a89e3cc481606ea7a
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2025-01-12 14:22:46 +0700

    menambahkan InsertBodyMahasiswa dan FormMahasiswa untuk UI input

--- app/src/main/java/com/example/week13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/week13/ui/view/InsertView.kt
@@ -1,7 +1,9 @@
 package com.example.week13.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -13,11 +15,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.week13.ui.viewModel.FormErrorState
 import com.example.week13.ui.viewModel.FormState
+import com.example.week13.ui.viewModel.InsertUiState
 import com.example.week13.ui.viewModel.InsertViewModel
+import com.example.week13.ui.viewModel.MahasiswaEvent
 import com.example.week13.ui.viewModel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -96,4 +102,33 @@ fun InsertMahasiswaView(
             )
         }
     }
+}
+
+@Composable
+fun InsertBodyMahasiswa(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMahasiswa(
+
+        )
+    }
+}
+
+@Composable
+fun FormMahasiswa(
+    mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
+    onValueChange: (MahasiswaEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+
 }
